1f961e61485f1ba7c87ca353dd74c101f9991da3
Your Name <you@example.com>
2022-07-08 12:59:03 +0530

    login logout system

---
+++ src/config.php
@@ -0,0 +1,18 @@
+<center>
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'mysql-server');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'secret');
+define('DB_NAME', 'demo');
+ 
+/* Attempt to connect to MySQL database */
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+?>
+</center>
